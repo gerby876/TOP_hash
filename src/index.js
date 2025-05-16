7969ba181628e22e7f756109fc681c9464a4b01b
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-05-16 12:04:12 -0400

    Add all functions odin asked for

--- src/index.js
+++ src/index.js
@@ -19,39 +19,102 @@ class HashMap {
     return this.map[h % this.map.length];
   }
 
-  entry(bucket, key) {
-    console.log(bucket);
+  entry(bucket) {
     for (let e of bucket) {
-      if (!e.contains(key)) {
-        return e;
-      }
-      return null;
+      e;
+      return e;
     }
+    return false;
   }
 
   set(key, value) {
-    if (!value) {
-      value = this.hash(key);
-    }
     let b = this.bucket(key);
-    let e = this.entry(b, key);
-    if (e) {
-      e.append(value);
+    let e = this.entry(b);
+    if (!e) {
+      const list = new LinkedList();
+      list.prepend(key, value);
+      b.push(list);
       return;
     }
-    const list = new LinkedList();
-    list.prepend(value);
-    b.push(list);
+    if (e.contains(key)) {
+      let y = e.find(key);
+      y.value = value;
+      return;
+    }
+    e.append(key, value);
   }
 
   get(key) {
     let b = this.bucket(key);
-    let e = this.entry(b, key);
-    if (e) {
-      console.log(e.find());
-      return e.data;
+    let e = this.entry(b);
+    if (e.contains(key)) {
+      let y = e.find(key);
+      return y.value;
     }
     return null;
+  }
+
+  has(key) {
+    let b = this.bucket(key);
+    let e = this.entry(b);
+    if (e.contains(key)) {
+      return true;
+    }
+    return false;
+  }
+
+  remove(key) {
+    let b = this.bucket(key);
+    let e = this.entry(b);
+    if (e.contains(key)) {
+      e.removeAt(key);
+      return true;
+    }
+    return false;
+  }
+
+  length() {
+    let total = 0;
+    for (let x = 0; x < this.map.length; x++) {
+      if (this.map[x].length > 0) {
+        total += this.map[x][0].size();
+      }
+    }
+    return total;
+  }
+
+  clear() {
+    this.map = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
+  }
+
+  keys() {
+    let all = [];
+    for (let x = 0; x < this.map.length; x++) {
+      if (this.map[x].length > 0) {
+        this.map[x][0].toArray(0, all);
+      }
+    }
+    return all;
+  }
+
+  values() {
+    let all = [];
+    for (let x = 0; x < this.map.length; x++) {
+      if (this.map[x].length > 0) {
+        this.map[x][0].toArray(1, all);
+      }
+    }
+    return all;
+  }
+
+  entries() {
+    let all = [];
+    for (let x = 0; x < this.map.length; x++) {
+      if (this.map[x].length > 0) {
+        this.map[x][0].toArray(2, all);
+      }
+    }
+    return all;
   }
 }
 
@@ -61,9 +124,19 @@ class LinkedList {
     this.tail = null;
   }
 
-  prepend(value) {
-    const newNode = new Node(value, this.head);
-
+  prepend(key, value) {
+    let contain = this.contains(key);
+    if (contain) {
+      let tmp = this.head;
+      let x = 0;
+      while (tmp != null) {
+        if (tmp.key == key) {
+          this.value == value;
+          return;
+        }
+      }
+    }
+    const newNode = new Node(key, value, this.head);
     this.head = newNode;
     if (this.tail !== null) {
       this.tail = this.tail;
@@ -72,8 +145,8 @@ class LinkedList {
     }
   }
 
-  append(value) {
-    const newNode = new Node(value, null);
+  append(key, value) {
+    const newNode = new Node(key, value, null);
     if (this.head == null) {
       this.head = newNode;
       this.tail = newNode;
@@ -83,25 +156,49 @@ class LinkedList {
     this.tail = newNode;
   }
 
-  size() {
+  contains(key) {
     let tmp = this.head;
-    let s = 0;
     while (tmp != null) {
+      if (tmp.key == key) {
+        return true;
+      }
       tmp = tmp.next;
-      s++;
     }
-    return s;
+    return false;
   }
 
-  at(index) {
+  find(key) {
     let tmp = this.head;
-    for (let x = 0; x < index; x++) {
-      tmp = tmp.next;
-      if (tmp == null) {
-        return alert("Node not found.");
+    let x = 0;
+    while (tmp != null) {
+      if (tmp.key == key) {
+        return tmp;
       }
+      x++;
+      tmp = tmp.next;
     }
-    return tmp;
+    return false;
+  }
+
+  removeAt(key) {
+    let x = 0;
+    let tmp = this.head;
+    if (tmp.key == key) {
+      this.head = tmp.next;
+      return;
+    }
+    while (tmp != null) {
+      if (tmp.next.key == key) {
+        break;
+      }
+      tmp = tmp.next;
+      x++;
+    }
+    if (tmp.next.next == null) {
+      this.pop();
+      return;
+    }
+    tmp.next = tmp.next.next;
   }
 
   pop() {
@@ -122,82 +219,73 @@ class LinkedList {
     tmp.next = null;
   }
 
-  contains(value) {
+  size() {
     let tmp = this.head;
+    let s = 0;
     while (tmp != null) {
-      if (tmp.data == value) {
-        return true;
-      }
       tmp = tmp.next;
+      s++;
     }
-    return false;
+    return s;
   }
 
-  find(value) {
-    let tmp = this.head;
-    let x = 0;
-    while (tmp != null) {
-      if (tmp.data == value) {
-        return x;
-      }
-      x++;
-      tmp = tmp.next;
-    }
-    return false;
-  }
-
-  toString() {
+  toArray(y, array) {
     let size = this.size();
     let tmp = this.head;
-    let output = "";
-    let newout = "";
-    for (let x = 0; x < size; x++) {
-      newout = output + `(${tmp.data}) -> `;
-      output = newout;
-      tmp = tmp.next;
-      if (tmp == null) {
-        newout = output + "null";
+    if (y == 0) {
+      for (let x = 0; x < size; x++) {
+        array.push(tmp.key);
+        if (tmp.next == null) {
+          return array;
+        } else {
+          tmp = tmp.next;
+        }
       }
     }
-    return newout;
-  }
-
-  insertAt(value, index) {
-    const newNode = new Node(value);
-    let tmp = this.head;
-    for (let x = 0; x < index; x++) {
-      if (x === index - 1) {
-        newNode.next = tmp.next;
-        tmp.next = newNode;
+    if (y == 1) {
+      for (let x = 0; x < size; x++) {
+        array.push(tmp.value);
+        if (tmp.next == null) {
+          return array;
+        } else {
+          tmp = tmp.next;
+        }
       }
-      tmp = tmp.next;
     }
-  }
-
-  removeAt(index) {
-    let x = 0;
-    let tmp = this.head;
-    while (x < index - 1) {
-      tmp = tmp.next;
-      x++;
+    if (y == 2) {
+      for (let x = 0; x < size; x++) {
+        array.push([tmp.key, tmp.value]);
+        if (tmp.next == null) {
+          return array;
+        } else {
+          tmp = tmp.next;
+        }
+      }
     }
-    tmp.next = tmp.next.next;
   }
 }
 
 class Node {
-  constructor(data, next) {
-    this.data = data;
+  constructor(key, value, next) {
+    this.key = key;
+    this.value = value;
     this.next = next;
   }
 }
 
 let newmap = new HashMap();
 
-newmap.set("Sara");
-newmap.set("raSa");
-newmap.set("Sara");
+newmap.set("Sara", "Hello");
+newmap.set("raSa", "Goodbye");
+newmap.set("Sara", "Hi");
+newmap.set("Roma", "Dog");
 
-newmap.get("raSa");
+console.log(newmap.get("Sara"));
+console.log(newmap.has("Sara"));
+// console.log(newmap.remove("Sara"));
+console.log(newmap.length());
+console.log(newmap.keys());
+console.log(newmap.values());
+console.log(newmap.entries());
 
 console.log(newmap);
